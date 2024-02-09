dd0e85d2a9e17a930f31b876b2ba3ea31cec7484
serafeimgr <serafeimgr@gmail.com>
2024-02-10 00:19:30 +0200

    feat(dotfiles) add dracula theme

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -3,3 +3,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+
+git clone https://github.com/dracula/zsh.git && ln -s ~/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
+
